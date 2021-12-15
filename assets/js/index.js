9f86302cf28745d89c513ba305cd54a1883dc9ff
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2021-12-15 14:23:54 -0300

    Share button / Share section done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,20 @@
+const shareButton = document.querySelector('.share-button');
+const imgShareButton = document.querySelector('.share-button img');
+const articleProfile = document.querySelector('.main-card__article');
+const articleShare = document.querySelector('.share-infos');
 
-
-
-
-
-
-
-
-
+shareButton.addEventListener('click', () => {
+  shareButton.classList.toggle('active');
+  if (shareButton.classList.contains('active')) {
+    shareButton.style.backgroundColor = '#6E8098';
+    imgShareButton.src = './assets/images/icon-share-active.svg';
+    articleProfile.style.display = 'none';
+    articleShare.style.display = 'flex';
+  }
+  else {
+    imgShareButton.src = './assets/images/icon-share.svg';
+    shareButton.style.backgroundColor = 'var(--light-grayish-blue)';
+    articleProfile.style.display = 'flex';
+    articleShare.style.display = 'none';
+  }
+});
